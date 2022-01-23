3ad5940305772fae61469a264876c2f0035d83a8
Nauris Pūķis <hi@pyronaur.com>
2022-01-23 17:41:05 +0200

    List scripts defined in package.json

---
+++ packls.mjs
@@ -0,0 +1,16 @@
+#!/usr/bin/env zx
+// List all the scripts defined in the package.json file
+
+if( ! fs.pathExistsSync( './package.json')) {
+	console.log("Can't find package.json in the current directory");
+	process.exit(0);
+}
+
+const contents = await fs.readFile('./package.json', 'utf8');
+const json = JSON.parse(contents);
+
+for( const script of Object.keys( json.scripts ) ) {
+	const command = json.scripts[script];
+	console.log(chalk.bold(script) + "\n" + command);
+	console.log();
+}
