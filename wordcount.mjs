275089a9a94ae9c822fe245e6fe55e9ef32f5c5c
Nauris Pūķis <hi@pyronaur.com>
2022-02-05 11:03:35 +0200

    Simple clipboard word counter

---
+++ wordcount.mjs
@@ -0,0 +1,6 @@
+#!/usr/bin/env zx
+const text = await $`pbpaste`
+
+console.log(chalk.dim("--------------------------------------------"))
+const length = text.stdout.match(/[a-z][a-z']+/gi).length
+console.log(chalk.bold("Word Count:"), chalk.bold.green(length))
