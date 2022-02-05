b377427356f5cffa63097be2287ef04bde2a8cbb
Nauris Pūķis <hi@pyronaur.com>
2022-02-05 12:41:36 +0200

    chalk.dim doesn't work well with Warp terminal

--- wordcount.mjs
+++ wordcount.mjs
@@ -1,6 +1,6 @@
 #!/usr/bin/env zx
 const text = await $`pbpaste`
 
-console.log(chalk.dim("--------------------------------------------"))
+console.log(chalk.gray("--------------------------------------------"))
 const length = text.stdout.match(/[a-z][a-z']+/gi).length
 console.log(chalk.bold("Word Count:"), chalk.bold.green(length))
