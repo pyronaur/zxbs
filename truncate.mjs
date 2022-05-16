f7a1cd6029e6b9937e2c936e89f3877a8e9bebe4
pyronaur <hi@pyronaur.com>
2022-05-16 14:16:26 +0300

    Add truncate

---
+++ truncate.mjs
@@ -0,0 +1,15 @@
+#!/usr/bin/env zx
+const file = argv._[1];
+
+if (!file) {
+  console.log("Usage: truncate <file>");
+  exit(1);
+}
+
+console.log(`Truncating ${file}`);
+const shouldTruncate = await question(`Continue? (Y/n) `);
+if (shouldTruncate.toLowerCase() === "n") {
+  exit(0);
+}
+
+fs.writeFileSync(file, "");
