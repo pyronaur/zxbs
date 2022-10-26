f8954b1aeaabc408e6b720508de3f161522de2c9
Nauris Pūķis <hi@pyronaur.com>
2022-10-26 10:47:12 +0300

    Truncate large json files

---
+++ truncate-json.mjs
@@ -0,0 +1,20 @@
+#!/usr/bin/env zx
+const file = argv._[0];
+const json = await fs.readJSON(file);
+
+function truncate(value) {
+	if (typeof value === 'string') {
+		return value.slice(0, 100);
+	}
+	if (Array.isArray(value)) {
+		return value.map(truncate);
+	}
+	if (typeof value === 'object' && value !== null && value !== undefined) {
+		return Object.fromEntries(
+			Object.entries(value).map(([key, value]) => [key, truncate(value)])
+		);
+	}
+	return value;
+}
+
+await fs.writeJSON(`${file}-truncated `, truncate(json), {spaces: 2});
