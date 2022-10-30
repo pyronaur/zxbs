5c23f1eb8f3fdc7de56121513e408e1899bf57dc
Nauris Pūķis <hi@pyronaur.com>
2022-10-30 08:47:32 +0200

    Publish blog Markdown blog posts

--- publish.mjs
+++ publish.mjs
@@ -1,26 +1,29 @@
 #!/usr/bin/env zx
-const file = argv._[0];
+// If there's an uncommitted markdown file, commit it and push it to GitHub.
 
-if (!file) {
-	throw new Error("You must specify a file to publish\nUsage: publish <file>");
+cd(`${os.homedir()}/Projects/Sites/pyronaur.com`)
+
+// Get uncommitted markdown files
+const uncommitted = await $`git status --porcelain`
+const files = uncommitted.toString().split(`\n`).filter(Boolean)
+if (files.length > 1) {
+	console.log(`Extra uncommitted files found, aborting.`);
+	process.exit(1);
 }
 
-if (! await fs.pathExistsSync('publish.yml')) {
-	throw new Error("Can't find publish.yml");
+// Get the file name
+const [status, file] = files[0].split(` `);
+if (status !== `??`) {
+	console.log(`Uncommitted file found, aborting.`);
+	process.exit(1);
 }
 
-if (! await fs.pathExistsSync(file)) {
-	throw new Error(`Can't find the file to publish: "${file}"`);
-}
+// Get the file contents
+const contents = await fs.readFile(file, `utf8`);
 
-const publish = YAML.parse(await fs.readFile('publish.yml', 'utf8'));
-
-if (!publish.destination) {
-	throw new Error("Can't find the publish destination");
-}
-
-fs.ensureDirSync(publish.destination);
-const filename = path.basename(file);
-const destFile = `${publish.destination}/${filename}`;
-console.log(`Copying ${filename} to ${destFile}`);
-await $`cp ${file} ${destFile}`
+// Get the title
+const title = contents.match(`title: (.*)`)[1];
+const commit = `Add ${title}`;
+await $`git add ${file}`;
+await $`git commit -m ${commit}`;
+await $`git push`
