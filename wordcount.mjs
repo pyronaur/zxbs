236f537f9b5eee2372969140837b9b5ed4d28211
Nauris Pūķis <hi@pyronaur.com>
2023-02-09 14:32:03 +0200

    Add wordcount whitespace

--- wordcount.mjs
+++ wordcount.mjs
@@ -1,6 +1,6 @@
 #!/usr/bin/env zx
 const text = await $`pbpaste`
 
-console.log(chalk.gray("--------------------------------------------"))
+console.log(chalk.gray("\n--------------------------------------------"))
 const length = text.stdout.match(/[a-z][a-z']+/gi).length
 console.log(chalk.bold("Word Count:"), chalk.bold.green(length))
