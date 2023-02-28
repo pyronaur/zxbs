3c9bd93dd779ebdd33836d64f1cb67560915f61c
Nauris Pūķis <hi@pyronaur.com>
2023-03-01 00:15:23 +0200

    Wordcount

--- wordcount.mjs
+++ wordcount.mjs
@@ -2,5 +2,11 @@
 const text = await $`pbpaste`
 
 console.log(chalk.gray("\n--------------------------------------------"))
+console.log(text);
+console.log(chalk.gray("--------------------------------------------"))
 const length = text.stdout.match(/[a-z][a-z']+/gi).length
+// Word Count
 console.log(chalk.bold("Word Count:"), chalk.bold.green(length))
+
+// Character Count
+console.log(chalk.bold("Character Count:"), chalk.bold.green(text.stdout.length))
