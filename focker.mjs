5cf2297cf5e0bc0b1e9323ba8c8f596b96ed8116
Nauris Pūķis <hi@pyronaur.com>
2024-04-10 09:31:14 +0300

    update focker

--- focker.mjs
+++ focker.mjs
@@ -1,40 +1,21 @@
-#!/usr/bin/env zx
 $.verbose = false;
-
-async function confirm(q, defaultAnswer = "n") {
-  if (argv.y) {
-    return true;
-  }
-  let yes_no = `(y/N)`;
-  if (defaultAnswer === "y") {
-    yes_no = `(Y/n)`;
-  }
-
-  let answer = await question(`${q} ${yes_no} `);
-
-  if (!answer) {
-    answer = defaultAnswer;
-  }
-
-  return "y" === answer;
-}
 
 function log() {
   console.log(chalk.green("[Focker] "), ...arguments);
 }
 
 function split(result) {
-  if (result.stdout === "") {
+  if (result === "") {
     return [];
   }
-  return result.stdout.split("\n");
+  return result.split("\n");
 }
 
 async function dockerInfo() {
-  let containers = await $`docker ps -qa`;
-  let images = await $`docker images -q`;
-  let volumes = await $`docker volume ls -q`;
-  let networks = await $`docker network ls -q`;
+  let containers = await $`docker ps -qa`.text();
+  let images = await $`docker images -q`.text();
+  let volumes = await $`docker volume ls -q`.text();
+  let networks = await $`docker network ls -q`.text()
 
   return {
     containers: split(containers),
@@ -56,39 +37,37 @@ await logInfo();
 
 const { containers, images, volumes, networks } = await dockerInfo();
 
-if (
-  !(await confirm("Are you sure you want to nuke all your docker images?", "y"))
-) {
+if (!ack("Are you sure you want to nuke all your docker images?", "y")) {
   log("Aborting");
   process.exit(0);
 }
-
+console.log(networks, containers.length);
 if (containers.length > 0) {
   log("Stopping containers");
-  await $`docker stop ${containers}`;
+  await $`docker stop ${containers}`.quiet();
 
   log("Removing containers");
-  await $`docker rm -f ${containers}`;
+  await $`docker rm -f ${containers}`.quiet();
 }
 
 if (images.length > 0) {
   log("Removing all images");
-  await $`docker rmi -f ${images}`;
+  await $`docker rmi -f ${images}`.quiet();
 }
 
 if (volumes.length > 0) {
   log("Removing all volumes", volumes);
-  await $`docker volume rm -f ${volumes}`;
+  await $`docker volume rm -f ${volumes}`.quiet();
 }
 
 if (networks.length > 0) {
   log("Removing all networks");
-  await nothrow($`docker network rm ${networks}`);
+  await $`docker network rm ${networks}`.quiet().nothrow();
 }
 
 // Maybe prune docker as well?
-if (await confirm("Do you want to prune docker?", "y")) {
-  await $`docker system prune -f`;
+if (ack("Do you want to prune docker?", "y")) {
+  await $`docker system prune -f`.quiet();
 }
 
 log("Done");
